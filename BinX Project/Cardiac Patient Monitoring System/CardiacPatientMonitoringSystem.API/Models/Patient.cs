eb3819cf5ee92c8717522cc12400b65b50cb7829
MohammadSalameh10 <hamodesalameh747@gmail.com>
2026-08-13 15:53:20 +0300

    Add core cardiac domain models

---
+++ BinX Project/Cardiac Patient Monitoring System/CardiacPatientMonitoringSystem.API/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace CardiacPatientMonitoringSystem.API.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string BloodType { get; set; } = string.Empty;
+    }
+}
