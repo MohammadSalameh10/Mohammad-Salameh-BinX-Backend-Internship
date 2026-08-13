d3cdb0786ff4d187e2e40b73155628adc2454db4
MohammadSalameh10 <hamodesalameh747@gmail.com>
2026-08-13 16:56:28 +0300

    Add patient DTOs

---
+++ BinX Project/Cardiac Patient Monitoring System/CardiacPatientMonitoringSystem.API/DTOs/Requests/CreatePatientRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace CardiacPatientMonitoringSystem.API.DTOs.Requests
+{
+    public class CreatePatientRequest
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string BloodType { get; set; } = string.Empty;
+    }
+}
