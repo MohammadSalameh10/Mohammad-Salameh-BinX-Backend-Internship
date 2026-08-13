924f1bc7adf5ce5ac8f8890e9aa2cf743007989c
MohammadSalameh10 <hamodesalameh747@gmail.com>
2026-08-13 16:08:48 +0300

    Add application database context

---
+++ BinX Project/Cardiac Patient Monitoring System/CardiacPatientMonitoringSystem.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CardiacPatientMonitoringSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardiacPatientMonitoringSystem.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<VitalSign> VitalSigns { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
