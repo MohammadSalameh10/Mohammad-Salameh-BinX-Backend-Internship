19892a087d858107d5a7931de4a5bc827868fa61
MohammadSalameh10 <hamodesalameh747@gmail.com>
2026-08-26 13:24:46 +0300

    Week6-Day4: add registration transaction and rollback handling

---
+++ BinX Internship/Week 6/Day 4/Cardiac Patient Monitoring System/CardiacPatientMonitoringSystem.API/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CardiacPatientMonitoringSystem.API.DTOs.Responses
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+    }
+}
---
+++ BinX Internship/Week 6/Day 4/Cardiac Patient Monitoring System/CardiacPatientMonitoringSystem.API/DTOs/Responses/VitalSignResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CardiacPatientMonitoringSystem.API.DTOs.Responses
+{
+    public class VitalSignResponse
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int HeartRate { get; set; }
+        public int SystolicBloodPressure { get; set; }
+        public int DiastolicBloodPressure { get; set; }
+        public int OxygenSaturation { get; set; }
+        public DateTime RecordedAt { get; set; }
+    }
+}
